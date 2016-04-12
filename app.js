6d3b8db2468bcd2a121f5784383bd031325a35a2
pohua920 <pohua920@gmail.com>
2016-04-12 17:27:11 +0800

    更新成功介接lib

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index.js');
 var users = require('./routes/users');
 
 var app = express();
+
+
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://192.168.60.65/test')
+
+// 0 = disconnected
+// 1 = connected
+// 2 = connecting
+// 3 = disconnecting
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('connected', function() {
+    console.log('connected', mongoose.connection.readyState);
+});
+db.once('disconnected', function() {
+    console.log('disconnected', mongoose.connection.readyState);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// var pinglib = require('../node_modules/hr-api/interface/session.js');
+var pinglib = require('pinglib');
+
+// var sessionManager = require('pinglib');
+// var usersobj = pinglib.User;
+// sessionManager.
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
